2f44a33084ee2c44c54813ba3f5a940e45a08909
christianbermudez121 <110303421+christianbermudez121@users.noreply.github.com>
2025-02-12 22:51:47 -0600

    Proveedores

--- WebApplication2/Data/AppDbContext.cs
+++ WebApplication2/Data/AppDbContext.cs
@@ -13,5 +13,11 @@ namespace WebApplication2.Data
 
 
 
+<<<<<<< HEAD
+=======
+       public DbSet<Proveedor> Provedores { get; set; }
+
+
+>>>>>>> 9ecdc814041d2d2e3a22179c38645361e88f3561
     }
 }
---
+++ WebApplication2/Models/Proveedor.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Articulo { get; set; }
+    }
+}
